4f5b4fbb07804e8bf67c154dcc9248319b02ed6c
Victor Williams <vrwilliams719@gmail.com>
2020-01-17 10:00:32 -0500

    a little extra before i go

--- draw.js
+++ draw.js
@@ -2,4 +2,7 @@ const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext('2d');
 const scale = 10;
 
-const rows
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
+
+var snake;
